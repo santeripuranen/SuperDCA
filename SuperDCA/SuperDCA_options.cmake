95f2d7137f6fb123a84088ed0529a723cf4aa45a
Santeri Puranen <26481940+santeripuranen@users.noreply.github.com>
2017-08-23 17:17:06 +0300

    Change default option

--- SuperDCA/SuperDCA_options.cmake
+++ SuperDCA/SuperDCA_options.cmake
@@ -9,4 +9,4 @@ option( SUPERDCA_ENABLE_LAPACK "Find LAPACK and, if successful, enable use in SU
 option( SUPERDCA_ENABLE_MPI "Find MPI and, if successful, enable use in SUPERDCA" false ) # off by default
 option( SUPERDCA_ENABLE_COMPILER_INTRINSICS "Find compiler intrinsics headers and, if successful, enable use in SUPERDCA" true )
 option( SUPERDCA_ENABLE_GPROF "Generate instrumented binaries for profiling with gprof" false ) # off by default
-option( SUPERDCA_ENABLE_DOXYGEN "Find Doxygen and enable documentation generation" true ) 
+option( SUPERDCA_ENABLE_DOXYGEN "Find Doxygen and enable documentation generation" false ) 
