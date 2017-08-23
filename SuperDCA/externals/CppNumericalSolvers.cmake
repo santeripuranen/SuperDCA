254addbc7b1fa6b86c83ec51c57c83dd14e7c9b6
Santeri Puranen <26481940+santeripuranen@users.noreply.github.com>
2017-08-23 18:30:21 +0300

    Changes needed for CppNumericalSolvers integration into SuperDCA

--- SuperDCA/externals/CppNumericalSolvers.cmake
+++ SuperDCA/externals/CppNumericalSolvers.cmake
@@ -12,9 +12,18 @@ set( SUPERDCA_NO_CPPNUMERICALSOLVERS true CACHE INTERNAL "Don't use CppNumerical
 
 if( SUPERDCA_ENABLE_CPPNUMERICALSOLVERS )
 	superdca_message( "check for CppNumericalSolvers" )
-	set( CPPNUMERICALSOLVERS_NO_TESTING true )
+	#set( CPPNUMERICALSOLVERS_NO_TESTING true )
 	add_definitions( -DNDEBUG )
-	add_subdirectory( CppNumericalSolvers )
+	#add_subdirectory( CppNumericalSolvers )
+	# CppNumericalSolvers is not a CMake project any more, so we'll just define these (needed by SuperDCA) here 
+
+	# Eigen3 Headerfiles
+	find_package(Eigen3 REQUIRED)
+	include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
+
+	# Definitions needed for SuperDCA integration
+	set( CPPNUMERICALSOLVERS_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/CppNumericalSolvers/include CACHE INTERNAL "CppNumericalSolvers include dir" )
+	set( CPPNUMERICALSOLVERS_FOUND true CACHE INTERNAL "CppNumericalSolvers found, if true" )
 
 	if( CPPNUMERICALSOLVERS_FOUND )
 		set( SUPERDCA_NO_CPPNUMERICALSOLVERS false CACHE INTERNAL "Don't use CppNumericalSolvers, if true" )
