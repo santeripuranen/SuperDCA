3416d74434af4ad185a008608bbbddac41c26b4b
Santeri Puranen <26481940+santeripuranen@users.noreply.github.com>
2018-04-09 13:14:23 +0300

    Bump version number due to bugfix

--- SuperDCA/include/SuperDCA_version.h
+++ SuperDCA/include/SuperDCA_version.h
@@ -27,7 +27,7 @@ struct SuperDCA_version
 {
 	static const int s_MajorVersion = 0; // substantial rewrite
 	static const int s_MinorVersion = 1; // feature change
-	static const int s_SubminorVersion = 0; // bugfix, small enhancement
+	static const int s_SubminorVersion = 1; // bugfix, small enhancement
 };
 
 } // namespace superdca
